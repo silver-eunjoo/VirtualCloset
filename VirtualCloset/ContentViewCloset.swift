75b2fada6421986f69a874cdc19beaed80978075
silver-eunjoo <leeeun5423@naver.com>
2022-05-21 14:39:31 +0900

    create closet navigation bar

--- VirtualCloset/ContentViewCloset.swift
+++ VirtualCloset/ContentViewCloset.swift
@@ -8,6 +8,63 @@
 import SwiftUI
 
 struct ContentViewCloset: View {
+    
+    var body: some View {
+        NavigationView() {
+            VStack {
+                NavigationLink(destination: SpringClothes()) {
+                    Image(systemName: "leaf")
+                        .font(.title)
+                    Text("Spring Clothes")
+                        .font(.title)
+                }.navigationTitle("Clothes")
+                Spacer().frame(height:50)
+                NavigationLink(destination: SummerClothes()) {
+                    Image(systemName: "sun.max.fill")
+                        .font(.title)
+                    Text("Summer Clothes")
+                        .font(.title)
+                }.navigationTitle("Clothes")
+                Spacer().frame(height:50)
+                NavigationLink(destination: SummerClothes()) {
+                    Image(systemName: "wind")
+                        .font(.title)
+                    Text("Fall Clothes")
+                        .font(.title)
+                }.navigationTitle("Clothes")
+                Spacer().frame(height:50)
+                NavigationLink(destination: SummerClothes()) {
+                    Image(systemName: "snowflake")
+                        .font(.title)
+                    Text("Winter Clothes")
+                        .font(.title)
+                }.navigationTitle("Clothes")
+                Spacer().frame(height:50)
+            }
+            
+        }
+    }
+}
+
+struct SpringClothes: View {
+    var body: some View {
+        Text(verbatim: "ContentViewCar")
+    }
+}
+
+struct SummerClothes: View {
+    var body: some View {
+        Text(verbatim: "ContentViewCar")
+    }
+}
+
+struct FallClothes: View {
+    var body: some View {
+        Text(verbatim: "ContentViewCar")
+    }
+}
+
+struct WinterClothes: View {
     var body: some View {
         Text(verbatim: "ContentViewCar")
     }
